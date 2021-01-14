4bc5f195be444b3320f23a249af914c672e9ddd5
Mauricio Roh <59180265+MauriRoh@users.noreply.github.com>
2021-01-14 09:55:11 -0300

    Update settings.py

--- designorganapp/designorganapp/settings.py
+++ designorganapp/designorganapp/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(%%yo*o+u=y481*6l@$fknl#y&it7th5^5d4y@_vvh3@i8rsrz'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
